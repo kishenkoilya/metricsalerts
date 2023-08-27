88cc9712ab11942b0f9cd1c6c8aab43e482c2fa4
kishenkoilya <illad08@gmail.com>
2023-08-27 18:47:10 +0300

    Splitting code

---
+++ cmd/server/flags.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/caarlos0/env/v6"
+)
+
+type Config struct {
+	Address       string `env:"ADDRESS"`
+	StoreInterval int    `env:"STORE_INTERVAL"`
+	FilePath      string `env:"FILE_STORAGE_PATH"`
+	Restore       bool   `env:"RESTORE"`
+	DatabaseDSN   string `env:"DATABASE_DSN"`
+}
+
+func getVars() (string, int, string, bool, string) {
+	addr := flag.String("a", "localhost:8080", "An address the server will listen to")
+	storeInterval := flag.Int("i", 300, "A time interval for storing metrics in file")
+	filePath := flag.String("f", "/tmp/metrics-db.json", "Path to file where metrics will be stored")
+	restore := flag.Bool("r", true, "A flag that determines wether server will download metrics from file upon start")
+	psqlLine := flag.String("d", "", "A string that contains info to connect to psql")
+
+	flag.Parse()
+
+	var cfg Config
+
+	error := env.Parse(&cfg)
+	if error != nil {
+		log.Fatal(error)
+	}
+	if cfg.Address != "" {
+		addr = &cfg.Address
+	}
+	if _, err := os.LookupEnv("STORE_INTERVAL"); err {
+		storeInterval = &cfg.StoreInterval
+	}
+	if cfg.FilePath != "" {
+		filePath = &cfg.FilePath
+	}
+	if _, err := os.LookupEnv("RESTORE"); err {
+		restore = &cfg.Restore
+	}
+	if cfg.DatabaseDSN != "" {
+		psqlLine = &cfg.DatabaseDSN
+	}
+	return *addr, *storeInterval, *filePath, *restore, *psqlLine
+}
---
+++ cmd/server/pages.go
@@ -0,0 +1,263 @@
+package main
+
+import (
+	"compress/gzip"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/jackc/pgerrcode"
+	"github.com/julienschmidt/httprouter"
+	"github.com/kishenkoilya/metricsalerts/internal/memstorage"
+	"github.com/kishenkoilya/metricsalerts/internal/psqlinteraction"
+)
+
+func printAllPage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	handlerVars := r.Context().Value(HandlerVars{}).(*HandlerVars)
+	sugar.Infoln("printAllPage")
+	path := strings.Trim(r.URL.Path, "/")
+	if path != "" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte(handlerVars.storage.PrintAll()))
+	w.WriteHeader(http.StatusOK)
+}
+
+func getPage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	handlerVars := r.Context().Value(HandlerVars{}).(*HandlerVars)
+	sugar.Infoln("getPage")
+	mType := ps.ByName("mType")
+	mName := ps.ByName("mName")
+	body := ""
+
+	statusRes, err := validateValues(mType, mName)
+	if err != nil {
+		sugar.Errorln("validateValues error: ", err.Error())
+		// sugar.Errorln("validateValues error: ", err.Error())
+		http.Error(w, "Error validating type and name", statusRes)
+		return
+	}
+	statusRes, body = getValue(handlerVars.storage, mType, mName)
+	if statusRes != http.StatusOK {
+		// sugar.Errorln("getValue error: ", err.Error())
+		http.Error(w, "Error getting value", statusRes)
+		return
+	}
+
+	w.Write([]byte(body))
+	w.WriteHeader(statusRes)
+}
+
+func pingPostgrePage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	handlerVars := r.Context().Value(HandlerVars{}).(*HandlerVars)
+	sugar.Infoln("pingPostgrePage")
+	dbPingFunc := psqlinteraction.PingPSQL(*handlerVars.psqlConnectLine)
+	_, err := Retrypg(pgerrcode.OperatorIntervention, dbPingFunc)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func updatePage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	handlerVars := r.Context().Value(HandlerVars{}).(*HandlerVars)
+	sugar.Infoln("updatePage")
+	mType := ps.ByName("mType")
+	mName := ps.ByName("mName")
+	mVal := ps.ByName("mVal")
+	body := "Update successful"
+
+	statusRes, err := validateValues(mType, mName)
+	if err != nil {
+		// sugar.Errorln("validateValues error: ", err.Error())
+		http.Error(w, "Error validating type and name", statusRes)
+		return
+	}
+	metric := memstorage.NewMetric(mType, mName, mVal)
+	if metric == nil {
+		http.Error(w, "Error parsing value", http.StatusBadRequest)
+		return
+	}
+	statusRes, metric = handlerVars.storage.SaveMetric(metric)
+	if statusRes != http.StatusOK {
+		// sugar.Errorln("saveValue error: ", err.Error())
+		http.Error(w, "Error parsing value", statusRes)
+		return
+	}
+	statusRes = writeValue(handlerVars, mType, mName, mVal)
+	if statusRes != http.StatusOK {
+		// sugar.Errorln("saveValue error: ", err.Error())
+		http.Error(w, "Error writing value to storage", statusRes)
+		return
+	}
+	body += metric.StringMetric()
+	w.Write([]byte(body))
+	w.WriteHeader(statusRes)
+}
+
+func getJSONPage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	handlerVars := r.Context().Value(HandlerVars{}).(*HandlerVars)
+	sugar.Infoln("getJSONPage")
+	var statusRes int
+	var req memstorage.Metrics
+
+	reqBody := r.Body
+	if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+		var err error
+		reqBody, err = gzip.NewReader(reqBody)
+		if err != nil {
+			// sugar.Errorln("gzip.NewReader failed", err.Error())
+			http.Error(w, "gzip.NewReader failed", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewDecoder(reqBody).Decode(&req)
+	if err != nil {
+		http.Error(w, "json.Marshal failed", http.StatusBadRequest)
+		return
+	}
+	req.PrintMetric()
+
+	_, err = validateValues(req.MType, req.ID)
+	resp := &memstorage.Metrics{}
+	if err != nil {
+		http.Error(w, "json.Marshal failed", http.StatusBadRequest)
+		return
+	}
+
+	statusRes, resp = handlerVars.storage.GetMetrics(req.MType, req.ID)
+	if statusRes != http.StatusOK {
+		// sugar.Errorln("storage.GetMetrics failed: ", statusRes)
+		w.WriteHeader(statusRes)
+		return
+	}
+	resp.PrintMetric()
+
+	respJSON, err := json.Marshal(resp)
+	if err != nil {
+		// sugar.Errorln("json.Marshal failed: ", err.Error())
+		http.Error(w, "json.Marshal failed", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(statusRes)
+	w.Write(respJSON)
+}
+
+func updateJSONPage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	handlerVars := r.Context().Value(HandlerVars{}).(*HandlerVars)
+	sugar.Infoln("updateJSONPage")
+	var statusRes int
+	var req *memstorage.Metrics
+	w.Header().Set("Content-Type", "application/json")
+
+	reqBody := r.Body
+	if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+		var err error
+		reqBody, err = gzip.NewReader(reqBody)
+		if err != nil {
+			http.Error(w, "gzip.NewReader failed", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	err := json.NewDecoder(reqBody).Decode(&req)
+	if err != nil {
+		http.Error(w, "json.Marshal failed", http.StatusBadRequest)
+		return
+	}
+	// req.PrintMetrics()
+	mType := req.MType
+	mName := req.ID
+	_, err = validateValues(mType, mName)
+	if err != nil {
+		http.Error(w, "json.Marshal failed", http.StatusBadRequest)
+		return
+	}
+	statusRes, req = handlerVars.storage.SaveMetric(req)
+	if statusRes != http.StatusOK {
+		http.Error(w, "storage.SaveMetrics failed", statusRes)
+		return
+	}
+	var mVal string
+	if req.Delta != nil {
+		mVal = fmt.Sprint(req.Delta)
+	} else {
+		mVal = fmt.Sprint(req.Value)
+	}
+	statusRes = writeValue(handlerVars, mType, mName, mVal)
+	if statusRes != http.StatusOK {
+		// sugar.Errorln("saveValue error: ", err.Error())
+		http.Error(w, "Error writing value to storage", statusRes)
+		return
+	}
+	respJSON, err := json.Marshal(&req)
+	if err != nil {
+		http.Error(w, "gzip.NewReader failed", http.StatusInternalServerError)
+		return
+	}
+
+	sugar.Infoln(string(respJSON))
+
+	w.WriteHeader(statusRes)
+	w.Write(respJSON)
+}
+
+func massUpdatePage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	handlerVars := r.Context().Value(HandlerVars{}).(*HandlerVars)
+	sugar.Infoln("massUpdatePage")
+	var statusRes int
+	var req *[]memstorage.Metrics
+	w.Header().Set("Content-Type", "application/json")
+
+	reqBody := r.Body
+	if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
+		var err error
+		reqBody, err = gzip.NewReader(reqBody)
+		if err != nil {
+			http.Error(w, "gzip.NewReader failed", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	err := json.NewDecoder(reqBody).Decode(&req)
+	if err != nil {
+		http.Error(w, "json.Marshal failed", http.StatusBadRequest)
+		return
+	}
+	// for _, val := range *req {
+	// 	val.PrintMetric()
+	// }
+
+	statusRes, resp := handlerVars.storage.SaveMetrics(req)
+	if statusRes != http.StatusOK {
+		http.Error(w, "storage.SaveMetrics failed", statusRes)
+		return
+	}
+	fmt.Println("printing response: ")
+	for _, val := range *resp {
+		val.PrintMetric()
+	}
+	statusRes = writeValues(handlerVars, resp)
+	if statusRes != http.StatusOK {
+		http.Error(w, "writeValues failed", statusRes)
+		return
+	}
+	resp1 := (*resp)[0]
+	respJSON, err := json.Marshal(&resp1)
+	if err != nil {
+		http.Error(w, "json.Marshal failed", http.StatusInternalServerError)
+		return
+	}
+
+	sugar.Infoln(string(respJSON))
+
+	w.WriteHeader(statusRes)
+	w.Write(respJSON)
+}
